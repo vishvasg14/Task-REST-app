d887ff5822ae2d0f056d0000cd9e7db39a261ab7
DESKTOP-EN2VTKC\Vishvas Gohil <vishvasg14@gmail.com>
2024-09-13 12:21:43 +0530

    swagger ui added

---
+++ src/main/java/com/task_rest_app/configuration/openApiConfig.java
@@ -0,0 +1,14 @@
+package com.task_rest_app.configuration;
+
+//@SecurityScheme(
+//		name = "auth",
+//		in = SecuritySchemeIn.HEADER,
+//		type = SecuritySchemeType.HTTP,
+//		bearerFormat = "jwt",
+//		scheme = "bearer",
+//		description = "security desc"
+//		)
+
+public class openApiConfig {
+
+}
--- src/main/java/com/task_rest_app/controller/MainController.java
+++ src/main/java/com/task_rest_app/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,6 @@ import com.task_rest_app.dto.StudentReqDto;
 import com.task_rest_app.dto.StudentResDto;
 import com.task_rest_app.entity.User;
 import com.task_rest_app.services.UserService;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/home")
